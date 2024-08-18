3c3e835161f1ac0a76bb502ccfa1ccb08384f25f
Hus54in <mhussum@gmail.com>
2024-08-18 17:01:29 -0600

    changed a few things

--- App.js
+++ App.js
@@ -1,27 +1,35 @@
 import { StyleSheet, SafeAreaView, Text, View } from "react-native";
 import ChatsScreen from "./app/chats_screen";
+
+const DATA = [
+  { id: "1", title: "Item 1" },
+  { id: "2", title: "Item 2" },
+  { id: "3", title: "Item 3" },
+  // Add more items as needed
+];
+
 export default function App() {
   return (
-    <View>
-      <SafeAreaView style={styles.container}>
-        <Text style={styles.title}>ChatR</Text>
-      </SafeAreaView>
-
-      <ChatsScreen />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.title}>Chats</Text>
+      <ChatsScreen style={styles.item_view} data={DATA} />
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: "#03a5fc",
+    flex: 1, 
+    backgroundColor: "black",
   },
   title: {
     padding: 20,
     fontSize: 30,
     fontWeight: "bold",
     textAlign: "start",
-    color: "#fff",
-  },
+    color: "white",
+  }, 
+  item_view: {
+    padding: 10
+  }
 });
--- app/chats_screen.js
+++ app/chats_screen.js
@@ -1,51 +1,32 @@
 import React from "react";
 import { StyleSheet, Text, View, FlatList } from "react-native";
 
-const DATA = [
-  { id: "1", title: "Item 1" },
-  { id: "2", title: "Item 2" },
-  { id: "3", title: "Item 3" },
-  // Add more items as needed
-];
 
-export default function ChatsScreen() {
-  return <ListView />;
-}
 
-function ListView() {
-  return (
-    <FlatList
-      data={DATA}
+export default function ChatsScreen(data, styles) {
+  return  (
+    <FlatList styles={styles}
+      data={data.data}
       renderItem={renderItem}
       keyExtractor={(item) => item.id}
     />
   );
 }
 
+
 const renderItem = ({ item }) => (
   <View style={styles.item}>
-    <Text>{item.title}</Text>
+    <Text style={styles.text}>{item.title}</Text>
   </View>
 );
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-  },
-  topBar: {
-    backgroundColor: "darkblue",
-    padding: 20,
-    alignItems: "center",
-  },
-  topBarText: {
-    color: "#fff",
-    fontSize: 24,
-    fontWeight: "bold",
-  },
   item: {
     padding: 20,
     borderBottomWidth: 1,
-    borderBottomColor: "#ccc",
+    borderBottomColor: "white",
   },
+  text: {
+    color: "white"
+  }
 });
