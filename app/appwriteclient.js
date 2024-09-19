fba782e71bdb27b38a7cb2c8bd6c7cbed104b57a
Hus54in <100100161+Hus54in@users.noreply.github.com>
2024-09-19 14:05:47 -0600

    Update appwriteclient.js

--- app/appwriteclient.js
+++ app/appwriteclient.js
@@ -3,7 +3,7 @@ import { Client, Account, Databases, Users } from "react-native-appwrite";
 class AppwriteClient {
   static client = new Client()
     .setEndpoint("https://cloud.appwrite.io/v1")
-    .setProject("66c27c9900115c049a13")
+    .setProject("project ID")
     .setPlatform("com.endem.ChatR");
   static databaseid = '66c302157b5e7708824c';
   static collection_user = '66c306b2000e1ccc322c';
